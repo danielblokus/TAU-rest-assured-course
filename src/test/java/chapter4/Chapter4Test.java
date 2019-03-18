65085421a6494a1e8a38cd33025e24c379740fc0
Daniel Blokus <daniel.blokus@gmail.com>
2019-03-18 20:49:50 +0100

    Add new test method using Response spec

--- src/test/java/chapter4/Chapter4Test.java
+++ src/test/java/chapter4/Chapter4Test.java
@@ -10,10 +10,12 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 public class Chapter4Test {
 
     private static final String BASE_URI = "http://api.zippopotam.us/";
+    private static final String PLACE_NAME = "São Paulo";
 
     private static RequestSpecification requestSpecification;
     private static ResponseSpecification responseSpecification;
@@ -42,4 +44,18 @@ public class Chapter4Test {
                 .and()
                 .contentType(ContentType.JSON);
     }
+
+    @Test
+    public void checkResponseHttpStatusCodeAndJsonContentTypeWithResponseSpecification() {
+        given()
+            .spec(requestSpecification)
+        .when()
+            .get("br/01000-000")
+        .then()
+            .spec(responseSpecification)
+        .and()
+            .assertThat()
+                .body("places[0].'place name'", equalTo(PLACE_NAME));
+    }
+
 }
